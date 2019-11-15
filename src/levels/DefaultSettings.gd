91eea0d63f5a0ceba15febf01ee8bb55be80c8af
Trace Myers <18mm@protonmail.com>
2019-11-15 12:13:35 -0500

    adding separate settings per level

--- src/levels/DefaultSettings.gd
+++ src/levels/DefaultSettings.gd
@@ -1,7 +1,9 @@
 extends Node
 
 const boundary_bottom_right = Vector2(2848.0, 2000.0)
-const start_seeds : int = 0
+const start_seeds : int = 1
 
 func _ready():
 	get_parent().get_node("Shaun").get_node("Camera2D").limit_right = boundary_bottom_right.x
+
+
