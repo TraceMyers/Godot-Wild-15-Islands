57a8d7772e4a65ab2e322c114ccae6a95fb4d02f
Trace Myers <18mm@protonmail.com>
2019-11-16 22:05:15 -0500

    fixed door anim bug

--- src/blocks/gate.gd
+++ src/blocks/gate.gd
@@ -4,6 +4,7 @@ var path = "res://src/levels/"
 var z = 0
 var door_anim_timer : Timer 
 var body
+var already_opened : bool = false
 
 func _ready():
 	z=0
@@ -12,30 +13,31 @@ func _ready():
 	add_child(door_anim_timer)
 
 func _on_Area2D_body_entered(_body):
-	door_anim_timer.start(0.5)
-	$AnimationPlayer.play("OpenDoor")
-	body = _body
+	if z < 1 and _body.name == "Shaun" and not already_opened:
+		door_anim_timer.start(0.5)
+		$AnimationPlayer.play("OpenDoor")
+		body = _body
+		already_opened = true
 
 func _door_anim_timer_timeout():
-	if z <1 and body.name == "Shaun":
-		var x =0
-		for i in Events.levels:
-			if get_tree().get_current_scene().name ==i:
-				if Events.levels.size() != x :
-					x+=1
-					fade.raise()
-					# warning-ignore:return_value_discarded
-					fade.get_node("AnimationPlayer").play("fadeIn")
-					yield(fade.get_node("AnimationPlayer"),"animation_finished")
-					get_tree().change_scene(path+"Level%s"%String(Events.current_level + 1)+".tscn")
-				else:
-					fade.raise()
-					fade.get_node("AnimationPlayer").play("fadeIn")
-					yield(fade.get_node("AnimationPlayer"),"animation_finished")
+	var x =0
+	for i in Events.levels:
+		if get_tree().get_current_scene().name ==i:
+			if Events.levels.size() != x :
+				x+=1
+				fade.raise()
+				# warning-ignore:return_value_discarded
+				fade.get_node("AnimationPlayer").play("fadeIn")
+				yield(fade.get_node("AnimationPlayer"),"animation_finished")
+				get_tree().change_scene(path+"Level%s"%String(Events.current_level + 1)+".tscn")
+			else:
+				fade.raise()
+				fade.get_node("AnimationPlayer").play("fadeIn")
+				yield(fade.get_node("AnimationPlayer"),"animation_finished")
 # warning-ignore:return_value_discarded
-					get_tree().change_scene("res://src/main/menu.tscn")
-					x+=1
-			
-		z =1
+				get_tree().change_scene("res://src/main/menu.tscn")
+				x+=1
+		
+	z =1
 
 
