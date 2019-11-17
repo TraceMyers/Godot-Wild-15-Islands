5773815c8e7daac9a101ac8f17d428ec982c4b54
Trace Myers <18mm@protonmail.com>
2019-11-17 13:16:57 -0500

    pause menu

--- src/seed/Seed.gd
+++ src/seed/Seed.gd
@@ -5,5 +5,6 @@ func _ready():
 	
 func _on_body_entered(body):
 	if body.name == "Shaun" and not body.get_node("Inventory").full("seed"):
+		Audio.emit_signal("play_sound", "seed_pickup")
 		Events.emit_signal("seed_pickup")
 		get_parent().queue_free()
